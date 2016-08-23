5de382e9c2cbcf38cb88fceba27d3d40901b5185
akagaeng <akagaeng@gmail.com>
2016-08-23 16:20:07 +0900

    Allied template with for statement!

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,10 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now
-    )
-    published_date = models.DateTimeField(
-        blank=True, null=True
-    )
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(selfself):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
